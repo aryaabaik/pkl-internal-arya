97d40382fd29cbf5a1e893ed98d84694c5bb0920
aryaabaik <aryaadhitya158@gmail.com>
2025-12-15 15:51:21 +0700

    pklday1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/tentang', function () {
+    return view('tentang');
+});
+
+Route::get('/sapa/{nama?}', function ($nama = 'Semua') {
+    return "Halo, $nama! Selamat datang di Toko Online.";
+
 });
